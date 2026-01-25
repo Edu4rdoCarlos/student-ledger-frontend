873e6e895adac8f39b8d54a9390cef2a8d5fd3b0
Edu4rdoCarlos <edu4rdobarb0za@gmail.com>
2026-01-25 00:12:08 -0300

    refactor: remove comments

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -12,10 +12,8 @@ export default function LoginPage() {
       <div className="absolute inset-0 bg-gradient-to-br from-slate-50 via-blue-50 to-indigo-100 dark:from-slate-950 dark:via-slate-900 dark:to-indigo-950" />
       <div className="absolute inset-0 bg-[url('/grid.svg')] bg-center opacity-30 dark:opacity-20" />
 
-      {/* Left side - Information panel */}
       <LoginInfoPanel />
 
-      {/* Right side - Login form */}
       <div className="relative flex w-full items-center justify-center p-6 lg:w-1/2">
         <div className="w-full max-w-md">
           <LoginLogo />
